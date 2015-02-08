c282cad925ec0c6a58f4173d622bf9179c59e3bc
broggi_t <broggi_t@epitech.eu>
2015-02-08 19:33:16 +0100

    v1.0.0

---
+++ README.md
@@ -0,0 +1,36 @@
+# Annuaire80
+
+A simple services status checker written in node.js.  
+If you have multiple services using differents ports, it will give you those who are active and those who are inactive, based on the status of the port.
+
+## Installation
+
+```bash
+npm install --python=python2
+mv config.json.default config.json
+```
+
+## Usage
+
+Once it has been installed, you can use Annuaire80 with the following command :
+
+```javascript
+./index.js
+```
+
+This is an example of configuration file:
+
+```javascript
+{
+	"services": { // the list of services
+		"80": "Annuaire80" // the key is the port, the value is the display name
+	},
+	"port": 80, // the port used by Annuaire80
+	"title": "Annuaire80", // the title of the page
+	"time": 10 // the delay in seconds between two updates of the active services list
+}
+```
+
+### Author
+
+Emeraude
---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+var app = require('express')();
+var server = require('http').Server(app);
+var config = require('./config.json');
+var url = require('url');
+var services;
+var portsRange = '';
+
+for (i in config.services) {
+    ;
+}
+
+
+function getPorts() {
+    require('node-libnmap').nmap('scan', {
+	range: ['127.0.0.1'],
+	ports: '1-65535'
+    }, function(err, report) {
+	if (err) throw err;
+	services = [];
+	for (i in config.services) {
+	    service = {
+		port: i,
+		name: config.services[i],
+		active: false
+	    };
+	    for (j in report[0][0].ports)
+		if (report[0][0].ports[j].port == service.port)
+		    service.active = true;
+	    services.push(service);
+	}
+    });
+    setTimeout(getPorts, config.time * 1000);
+}
+
+getPorts();
+
+server.listen(config.port);
+app.engine('jade', require('jade').__express)
+    .use(require('compression')())
+    .use(require('serve-static')('public/'))
+    .get('*', function(req, res) {
+	parsedUrl = url.parse('http://' + req.headers.host);
+	res.render('home.jade', {title: config.title,
+				 services: services,
+				 location: parsedUrl.protocol + '//' + parsedUrl.hostname});
+    });
